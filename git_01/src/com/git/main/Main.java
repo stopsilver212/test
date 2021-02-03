afe9404d435d9f031f7e3cf7c869ef2eb0bfc48c
stopsilver212 <stopsilver212@gmail.com>
2021-02-03 17:09:17 +0900

    i wanna go home..

--- git_01/src/com/git/main/Main.java
+++ git_01/src/com/git/main/Main.java
@@ -5,11 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git!");
-<<<<<<< HEAD
 		System.out.println("Hello HTML!");
-=======
 		System.out.println("Hello Spring!");
->>>>>>> refs/heads/master
+		System.out.println("Hello jQuery!");
 	}
 
 }
